442d88032a9b75fc157ef20b5e2e3e203b8e99bc
adidongs1 <adidongs15@gmail.com>
2024-01-11 09:32:10 +0700

    Remove logo prop from Navbar component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children }) {
     return (
         <div className="flex flex-col min-h-screen">
             {/* Navbar */}
-            <Navbar logo={'./src/assets/logo.svg'} />
+            <Navbar />
 
             {/* main content */}
             <main className="mt-40">
