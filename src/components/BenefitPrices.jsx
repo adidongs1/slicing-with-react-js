da2c3ff2082233057fddc883a04ac4959f90e68f
adidongs1 <adidongs15@gmail.com>
2024-01-11 07:54:23 +0700

    Add BenefitPrices component

---
+++ src/components/BenefitPrices.jsx
@@ -0,0 +1,11 @@
+import CheckIcon from '../assets/check-circle-1.svg'
+function BenefitPrices({ title }) {
+    return (
+        <div className="flex gap-3 mb-3">
+            <img src={CheckIcon} alt="icon-check" />
+            <p className="m-0 text-[#848199]">{title}</p>
+        </div>
+    )
+}
+
+export default BenefitPrices
