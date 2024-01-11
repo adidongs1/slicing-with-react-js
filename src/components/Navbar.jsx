fe6d6552df4639b80efe049661eb89a7139567b5
adidongs1 <adidongs15@gmail.com>
2024-01-11 07:54:46 +0700

    Update Navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,9 @@ function Navbar({ logo }) {
                             </NavLink>
                             {/* Menu */}
                             <div className="hidden sm:flex items-center sm:gap-3 md:gap-5 text-base font-medium">
-                                <NavLink to="/Features">Features</NavLink>
-                                <NavLink to="/">Prices</NavLink>
-                                <NavLink to="/">About</NavLink>
-                                <NavLink to="/">Contacts</NavLink>
+                                <NavLink to="/prices">Prices</NavLink>
+                                <NavLink to="/about">About</NavLink>
+                                <NavLink to="/contact">Contacts</NavLink>
                                 <NavLink
                                     to="/"
                                     className="border-2 border-transparent py-[10px] px-5 bg-success rounded-lg text-white font-semibold hover:bg-transparent hover:border-2 hover:border-success hover:text-success"
