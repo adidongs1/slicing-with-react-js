42ec500e72894d6792e6e865938ee7bc6e6c28de
adidongs1 <adidongs15@gmail.com>
2024-01-13 00:47:20 +0700

    update import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import DataTestimonials from '../lib/data-testimonials';
+
 import CardService from "../components/CardService";
 import SellingPointsContent from "../components/SellingPointsContent";
 import CardTestimonials from "../components/CardTestimonials";
@@ -6,15 +8,18 @@ import Hero from "../components/Hero";
 import Layout from "./Layout";
 
 
-import panahKanan from "../assets/arrowright.svg";
-import varta from "../assets/varta.svg";
-import lenovo from "../assets/lenovo.svg";
-import bbs from "../assets/bbs.svg";
-import weller from "../assets/weller.svg";
-import british_airways from "../assets/british_airways.svg";
-import Lufthansa from "../assets/lufthansa.svg";
+import panahKanan from "../assets/icon/arrowright.svg";
+import varta from "../assets/icon/varta.svg";
+import lenovo from "../assets/icon/lenovo.svg";
+import bbs from "../assets/icon/bbs.svg";
+import weller from "../assets/icon/weller.svg";
+import british_airways from "../assets/icon/british_airways.svg";
+import Lufthansa from "../assets/icon/lufthansa.svg";
 
-import BlockPics from "../assets/block-pic.svg";
+import BlockPics from "../assets/icon/block-pic.svg";
+
+
+
 
 
 
@@ -25,35 +30,16 @@ function Partners() {
     return (
         <>
             {/* Section Partner */}
-            <section className="mt-28">
-                <div className="container mx-auto sm:px-4 py-5 mb-20">
-                    <div className="flex">
-                        <div className="md:w-1/5 pr-4 pl-4 w-1/3 mb-3 md:mb-0">
-                            <img className="w-40" src={varta} alt="varta" />
-                        </div>
-                        <div className="md:w-1/5 pr-4 pl-4 w-1/3 mb-3 md:mb-0">
-                            <img className="w-40" src={lenovo} alt="lenovo" />
-                        </div>
-                        <div className="md:w-1/5 pr-4 pl-4 w-1/3 mb-3 md:mb-0">
-                            <img className="w-40" src={bbs} alt="bbs" />
-                        </div>
-                        <div className="md:w-1/5 pr-4 pl-4 w-1/3 mb-3 md:mb-0">
-                            <img className="w-40" src={weller} alt="weller" />
-                        </div>
-                        <div className="md:w-1/5 pr-4 pl-4 w-1/3 mb-3 md:mb-0">
-                            <img
-                                className="w-40"
-                                src={british_airways}
-                                alt="british_airways"
-                            />
-                        </div>
-                        <div className="md:w-1/5 pr-4 pl-4 w-1/3 mb-3 md:mb-0">
-                            <img
-                                className="w-40"
-                                src={Lufthansa}
-                                alt="lufthansa"
-                            />
-                        </div>
+            <section className="sm:mt-28 mt-20">
+                <div className="container mx-auto sm:px-4 mb-20">
+                    <div className="grid grid-cols-3 gap-4 md:flex md:flex-wrap md:gap-8 md:justify-center">
+                        <img src={varta} alt="varta" className="w-40" />
+                        <img src={lenovo} alt="lenovo" className="w-40" />
+                        <img src={bbs} alt="bbs" className="w-40" />
+                        <img src={weller} alt="weller" className="w-40" />
+                        <img src={british_airways} alt="british_airways" className="w-40" />
+                        <img src={Lufthansa} alt="Lufthansa" className="w-40" />
+
                     </div>
                 </div>
             </section>
@@ -105,17 +91,17 @@ function SellingPoints() {
 function Service() {
     return (
         <>
-            <section className="min-h-[44rem] bg-[#F3F4F6] pt-32">
-                <div className="flex flex-col items-center justify-center mx-96">
-                    <h3 className="font-semibold text-5xl">Our service features</h3>
-                    <p className="text-center w-[27rem] mt-2">
+            <section className="min-h-[44rem] bg-[#F3F4F6] pt-10 sm:pt-32">
+                <div className="flex flex-col items-center p-8 sm:mx-auto">
+                    <h3 className="font-semibold text-5xl text-center">Our service features</h3>
+                    <p className="text-center sm:w-[27rem] mt-2">
                         Aliquid officiis cumque sunt sint. Et quo culpa. Enim
                         sed natus molestiae fugiat cum consequatur quia
                         sunt.
                     </p>
                 </div>
 
-                <div className="flex gap-10 p-16 justify-center">
+                <div className="flex flex-wrap sm:flex-nowrap gap-10 p-14 justify-center">
                     <CardService cardTitle="Incredible">
                         Adipisci tempora pariatur modi
                         recusandae. Omnis neque
@@ -156,44 +142,32 @@ function Service() {
 function Testimonials() {
     return (
         <>
-            <section className="min-h-[44rem]  pt-28">
-                <div className="flex flex-col items-center justify-center mx-96">
-                    <h3 className="font-semibold text-5xl">Testimonials</h3>
-                    <p className="text-center w-[27rem] mt-2">
+            <section className="min-h-[44rem] pt-10 sm:pt-32">
+                <div className="flex flex-col items-center p-8 sm:mx-auto">
+                    <h3 className="font-semibold text-5xl text-center">Testimonials</h3>
+                    <p className="text-center sm:w-[27rem] mt-2">
                         Lorem ipsum dolor sit amet consectetur adipisicing
                         elit. Quisquam, repellendus numquam.
                     </p>
                 </div>
 
 
-                <div className="flex gap-10 p-16 justify-center">
-                    <CardTestimonials name="Arya Sakti" jobDesk="CEO of Lapas Troops">
-                        Et quibusdam voluptatem molestias cum
-                        autem autem et ut. Ad et tenetur. Autem
-                        quis id tempora accusantium quod dolores
-                        et. Possimus voluptatem hic nulla
-                        consequatur voluptates libero quia
-                        expedita. Eum aut voluptatem qui
-                        praesentium vitae.
-                    </CardTestimonials>
-                    <CardTestimonials name="Bill Fedro" jobDesk="Co-Founder of Lapas Troops">
-                        Et quibusdam voluptatem molestias cum
-                        autem autem et ut. Ad et tenetur. Autem
-                        quis id tempora accusantium quod dolores
-                        et. Possimus voluptatem hic nulla
-                        consequatur voluptates libero quia
-                        expedita. Eum aut voluptatem qui
-                        praesentium vitae.
-                    </CardTestimonials>
-                    <CardTestimonials name="Arya Sakti" jobDesk="CEO of Lapas Troops">
-                        Et quibusdam voluptatem molestias cum
-                        autem autem et ut. Ad et tenetur. Autem
-                        quis id tempora accusantium quod dolores
-                        et. Possimus voluptatem hic nulla
-                        consequatur voluptates libero quia
-                        expedita. Eum aut voluptatem qui
-                        praesentium vitae.
-                    </CardTestimonials>
+                <div className="flex flex-wrap  gap-10 p-16 justify-center sm:justify-start">
+                    {
+                        DataTestimonials.testimonials.map((item) => {
+                            return (
+                                <CardTestimonials
+                                    name={item.name}
+                                    position={item.position}
+                                    imgProfile={item.image}
+                                    key={item.id}
+                                >
+
+                                    {item.comment}
+                                </CardTestimonials>
+                            )
+                        })
+                    }
 
                 </div>
 
@@ -207,16 +181,16 @@ function FreeTrials() {
         <>
             <div className="container">
                 <div className=" my-8 mx-8 h-[26rem] px-1 py-28 card bg-success rounded-lg place-items-center gap-12">
-                    <div className="flex flex-col items-center justify-center mx-96 text-white">
-                        <h3 className="font-semibold text-5xl">Start your free trial</h3>
-                        <p className="text-center w-[27rem] mt-2">
+                    <div className="flex flex-col items-center justify-center mx-auto text-white">
+                        <h3 className=" text-3xl font-bold sm:font-semibold  sm:text-5xl text-center">Start your free trial</h3>
+                        <p className="text-center sm:w-[27rem] mt-2">
                             Trial period - 14 days, no credit card required
                         </p>
                     </div>
 
-                    <div className="flex flex-col items-center justify-center mx-96">
+                    <div className="flex flex-col items-center justify-center mx-auto">
                         <div className="join">
-                            <input className="input input-bordered join-item w-72" placeholder="Your email address" />
+                            <input className="input input-bordered join-item sm:w-72" placeholder="Your email address" />
                             <button className="btn join-item bg-[#047857] border-none text-white">Get Started</button>
                         </div>
                     </div>
@@ -230,9 +204,9 @@ function Home() {
     return (
         <Layout>
 
-            <section className="container">
+            <section className="container z-0">
                 <Hero >
-                    <h1 className="text-6xl font-semibold pr-12">
+                    <h1 className="sm:text-6xl font-semibold pr-12 text-3xl">
                         Landing page for your online service
                     </h1>
                     <p className="py-6 text-lg pr-12">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae.</p>
