eefeb12d34cd1a958886d447b0a38dd0c5770973
adidongs1 <adidongs15@gmail.com>
2024-01-13 04:31:46 +0700

    Update Footer component styling for responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
                         <img src={Logo} alt="" />
                     </aside>
 
-                    <nav className="flex gap-3 text-base font-medium justify-self-center">
+                    <nav className="hidden sm:flex gap-3 text-base font-medium justify-self-center">
                         <NavLink to="/prices">Prices</NavLink>
                         <NavLink to="/about">About</NavLink>
                         <NavLink to="/contact">Contacts</NavLink>
@@ -21,7 +21,7 @@ function Footer() {
                     </nav>
 
 
-                    <nav className=" flex gap-4 justify-self-end">
+                    <nav className="flex gap-4 justify-self-end">
                         <a href="/"><img src={FacebookLogo} alt="facebook" /></a>
                         <a href="/"><img src={TwitterLogo} alt="twitter" /></a>
                     </nav>
@@ -30,7 +30,7 @@ function Footer() {
 
             <footer className="footer footer-center px-4 pb-4 bg-white">
                 <aside >
-                    <p className='text-center text-base-300 ml-24'> Copyright © 2024 Adi Arifin. All rights reserved.</p>
+                    <p className='text-center text-base-300 sm:ml-24'> Copyright © 2024 Adi Arifin. All rights reserved.</p>
                 </aside>
             </footer>
 
